8d9ef18f10acd4ac1513ab0a7547e6a57a5b80a6
Vic <victor.lisman@protonmail.com>
2022-12-09 18:20:01 +0200

    Training model

--- src/train.py
+++ src/train.py
@@ -56,9 +56,11 @@ batchSize = 8
 inputSize = len(X_train[0])
 hiddenSize = 8
 outputSize = len(tags)
+learningRate = 0.001
+numEpochs = 1000
 
-print(input, len(allWords))
-print(outputSize, tags)
+#print(input, len(allWords))
+#print(outputSize, tags)
 
 dataset = StockBotDataset()
 trainLoader = DataLoader(dataset=dataset, batch_size=batchSize, shuffle=True, num_workers=2)
@@ -67,3 +69,30 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNet(inputSize, hiddenSize, outputSize).to(device)
 
 #de facut learning rate si optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learningRate)
+
+
+def main():
+    for epoch in range(numEpochs):
+        for (words, labels) in trainLoader:
+            words = words.to(device)
+            labels = labels.to(dtype=torch.long).to(device)
+
+            outputs = model(words)
+            loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if (epoch + 1) % 100 == 0:
+            print(f'epoch {epoch + 1} / {numEpochs}, loss = {loss.item():.4f}')
+
+    print(f'final loss, loss = {loss.item():.4f}')
+
+# de facut rezolva RuntimeError: Function AddmmBackward returned an invalid gradient at index 2 - got [23, 3] but expected shape compatible with [8, 3]
+
+
+if __name__ == '__main__':
+    main()
